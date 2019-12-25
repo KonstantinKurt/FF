0cbbc45a2d4996911a0a74d36c20b32fa1ff00ce
KonstantinKurt <kurbatskykv@gmail.com>
2019-12-25 09:36:30 +0200

    front-end init

--- services/open-api/src/auth/auth.controller.ts
+++ services/open-api/src/auth/auth.controller.ts
@@ -1,14 +1,21 @@
-import {Body, Controller, HttpCode, HttpStatus, Logger, Post} from '@nestjs/common';
-import {AuthService} from "./auth.service";
+import {
+    Body,
+    Controller,
+    HttpCode,
+    HttpStatus,
+    Logger,
+    Post,
+} from '@nestjs/common';
+import {AuthService} from './auth.service';
 
 import {
     ApiOperation,
     ApiUseTags,
 
 } from '@nestjs/swagger';
-import {LoginDto} from "./dto/login.dto";
-import {IpAddressCheck} from "./decorators/check_ip.decorator";
-import {RegisterDto} from "./dto/register.dto";
+import {LoginDto} from './dto/login.dto';
+import {IpAddressCheck} from './decorators/check_ip.decorator';
+import {RegisterDto} from './dto/register.dto';
 
 @ApiUseTags('Auth Controller')
 @Controller(process.env.OPEN_API_AUTH_URL)
@@ -16,12 +23,12 @@ export class AuthController {
     private logger = new Logger('Open-API Auth-controller');
     constructor(
       private readonly authService: AuthService,
-    ){}
+    ) {}
 
     @Post('/login')
     @ApiOperation({title: 'login user'})
     @HttpCode(200)
-    async login(@Body() loginData: LoginDto,  @IpAddressCheck() ip: string){
+    async login(@Body() loginData: LoginDto,  @IpAddressCheck() ip: string) {
         this.logger.log(loginData);
         loginData.ip = ip;
         return this.authService.login(loginData);
@@ -30,10 +37,9 @@ export class AuthController {
     @Post('/register')
     @ApiOperation({title: 'register user'})
     @HttpCode(201)
-    async register(@Body() registerData: RegisterDto, @IpAddressCheck() ip: string){
+    async register(@Body() registerData: RegisterDto, @IpAddressCheck() ip: string) {
         registerData.ip = ip;
         return this.authService.register(registerData);
     }
-
 
 }
