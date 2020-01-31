428230e66f565cfe7795b16afe70db1b5e335890
KonstantinKurt <kurbatskykv@gmail.com>
2020-01-31 19:42:06 +0200

    Test

--- front/src/app/shared/custom-validator.validator.ts
+++ front/src/app/shared/custom-validator.validator.ts
@@ -1,4 +1,5 @@
 import { AbstractControl } from '@angular/forms';
+import {Observable} from "rxjs";
 
 export class CustomValidator {
   static passwordMatchValidator(control: AbstractControl) {
@@ -6,6 +7,14 @@ export class CustomValidator {
     const confirmPassword: string = control.get('confirmPassword').value;
     if (password !== confirmPassword) {
       control.get('confirmPassword').setErrors({ notMatch: true });
+    }
+  }
+  static emailUniqueValidator(control: AbstractControl) {
+    const email: string = control.get('email').value;
+    const check: Observable<boolean> = this.registerService.checkEmail(email);
+    console.log(check);
+    if (check) {
+      control.get('email').setErrors({alreadyExists: true});
     }
   }
 }
